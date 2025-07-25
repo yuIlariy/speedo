1331c4ad0d5468d1b4b77aa9fa80fce94f004e2c
Ilariy <147840605+yuIlariy@users.noreply.github.com>
2025-07-25 09:42:27 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 TOKEN = "your_actual_bot_token"
-ADMIN_ID = 123456789  # Replace with your Telegram user ID
+ADMIN_ID = 6318135266  # Replace with your Telegram user ID
 THUMBNAIL_URL = "https://telegra.ph/file/e292b12890b8b4b9dcbd1.jpg"
 LOG_FILE = "logs/bot.log"
