b986b5e5fee9a71e14bed7ad97167f822de0d916
Ilariy <147840605+yuIlariy@users.noreply.github.com>
2025-07-25 09:12:43 +0300

    Update speedtest_handler.py

--- handlers/speedtest_handler.py
+++ handlers/speedtest_handler.py
@@ -1,1 +1,7 @@
+from aiogram import types
+from utils.speedtest import run_speedtest
 
+async def handle_speedtest(message: types.Message):
+    await message.reply("Running speedtest... ⏳")
+    result = run_speedtest()
+    await message.reply(result)
