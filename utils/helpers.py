b5692b20066e1324913a27b7a829215ce2ce48f1
Ilariy <147840605+yuIlariy@users.noreply.github.com>
2025-07-26 10:17:12 +0300

    Create helpers.py

---
+++ utils/helpers.py
@@ -0,0 +1,16 @@
+import platform
+import psutil
+
+def get_sysinfo() -> str:
+    sys = platform.uname()
+    mem = psutil.virtual_memory()
+    disk = psutil.disk_usage("/")
+
+    return (
+        f"<b>🧠 System</b>: {sys.system} {sys.release} ({sys.machine})\n"
+        f"<b>🕹️ Uptime</b>: {round(psutil.boot_time() / 3600, 1)} hrs\n"
+        f"<b>💾 Memory</b>: {mem.used // (1024 ** 2)}MB / {mem.total // (1024 ** 2)}MB\n"
+        f"<b>📀 Disk</b>: {disk.used // (1024 ** 3)}GB / {disk.total // (1024 ** 3)}GB\n"
+        f"<b>⚙️ CPU</b>: {psutil.cpu_percent()}% used"
+    )
+
