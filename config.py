be83e3554db41f5d65135d3f7b389ffca09ef1da
Ilariy <147840605+yuIlariy@users.noreply.github.com>
2025-07-26 10:35:51 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,7 @@
+# config.py
+
+RESULTS_LOG = "results/speedlog.json"
+TREND_IMAGE = "results/speedplot.png"
 TOKEN = "your_actual_bot_token"
 ADMIN_ID = 6318135266  # Replace with your Telegram user ID
 THUMBNAIL_URL = "https://telegra.ph/file/e292b12890b8b4b9dcbd1.jpg"
